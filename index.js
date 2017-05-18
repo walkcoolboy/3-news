1df1921dbe0175c2efd4dcad57b7f7366c72836b
Li Li <walkcoolboyli@gmail.com>
2017-05-19 10:57:01 +1200

    add mongoDB driver

--- index.js
+++ index.js
@@ -6,9 +6,24 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
 var fs = require('fs');
-var path = require('path'); 
+var path = require('path');
 var pg = require('pg').native;
 var port = process.env.PORT || 8080;
+
+//MongoDB Connection
+var MongoClient = require('mongodb').MongoClient
+  , assert = require('assert');
+
+// Connection URL
+var url = 'mongodb://heroku_khwjm57z:5e7v1vdpgpluug4e3vd4cgm242@ds143131.mlab.com:43131/heroku_khwjm57z';
+
+// Use connect method to connect to the server
+MongoClient.connect(url, function(err, db) {
+  assert.equal(null, err);
+  console.log("Connected successfully to server");
+
+  db.close();
+});
 
 //-----------------------------------------
 //Middleware code starts here
@@ -28,7 +43,7 @@ app.use('/', express.static(__dirname + '/'));
 
 
 app.get('/', function (req, res) {
-    var filepath = path.join(__dirname, 'index.html');    
+    var filepath = path.join(__dirname, 'index.html');
     fs.readFile(filepath, function (err, data) {
       res.end(data);
     });
