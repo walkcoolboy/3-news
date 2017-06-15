a03f654ea8bfa279f9b6ce182e90ef11ee73c196
millerjona1 <millerjona1@ecs.vuw.ac.nz>
2017-06-15 13:11:48 +1200

    Small fix that may help the heroku issue

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.use(bodyParser.urlencoded({extended: false}));
 
 var path = require('path');
 //var pg = require('pg').native;
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 8443;
 
 var userController = require('./api/user');
 var articleController = require('./api/article');
@@ -54,7 +54,7 @@ app.use(function (req, res, next) {
 // });
 
 //HTTPS
-var server = https.createServer(https_options, app).listen(8443, 'localhost');
+var server = https.createServer(https_options, app).listen(port, 'localhost');
 
 //Hosts all files within the directory for access
 //Temporary measure for ease of use
