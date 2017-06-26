397e933bce8b3dfb61296e8c044c7cb687c1948f
~Clinton <normanclin@ecs.vuw.ac.nz>
2017-06-26 13:29:25 +1200

    fixed route index

--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,16 @@
+var articleController = require('../api/article');
+
 exports.index = function(req, res){
-	//do database trans for top content
-
-	res.render('app.ejs', {
-		title: '3 News - Top content',
-		header: "Top Content",
-		data: {
-			title: "Dummy article one top article",
-			description: "Short description of article in index."}
-	});
+var dbResults = articleController.getArticles()
+		.then((articles) =>{
+		if(!articles || !articles[0])return res.json("Article"+ id + " not found");
+			res.render('app.ejs', {
+				title: '3 News',
+				header: 'Top Content',
+				data: articles,
+			});
+		})
+		.catch((err) => {
+				res.json(err);
+		});
 };
-
-//moved to articles.category
-
-// exports.category = function(req, res){
-// 	res.render('app.ejs', {
-// 		title: '3 News - ' + req.params.tag,
-// 		header: req.params.tag,
-// 		data: {title: "Dummy article " + req.params.tag, description: "Short description of article in index."}//req.params.jsonData
-// 	});
-// };
