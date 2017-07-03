6a4d2a718fadb84b5ace76c56c1f869dcf1b8c82
Li Li <walkcoolboyli@gmail.com>
2017-07-03 17:49:13 +1200

    changed google OAuth from localhost to heroku

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 exports.auth = {
   'googleAuth' : {
-    'clientID' : '924537122442-cmo3ataq7nrv8tvaouiremsncm0igmc1.apps.googleusercontent.com',
-    'clientSecret'  : 'WnIXWqx3SnIjNvRBc1AaSmZa',
-    'callbackURL'   : 'https://localhost/auth/google/callback'
+    'clientID' : '887212931224-gduet7l9vvk58b75oovrcptkkgfdkbvs.apps.googleusercontent.com',
+    'clientSecret'  : 'n1q7TGJ3bR6DM44sRjKLPLZA',
+    'callbackURL'   : 'https://nwen304-3-news.herokuapp.com/auth/google/callback'
   }
 };
